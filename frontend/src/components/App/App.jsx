5e7278f799c4e75ee0b5ebed3062cbcf214ec874
artfront5 <art.front@list.ru>
2022-07-07 13:24:10 +0300

    init project, greate components, dependencies

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+function App(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
