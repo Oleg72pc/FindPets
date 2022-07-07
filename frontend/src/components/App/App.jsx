0bef6fee53298f5784a22235164ac92f10659a9a
artfront5 <art.front@list.ru>
2022-07-07 13:42:48 +0300

    Navbar

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,26 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home'
 import Footer from '../Footer/Footer';
 import Advert from '../Advert/Advert';
 import Support from '../Support/Support';
-import { store } from '../../redux/store';
+// import { store } from '../../redux/store';
 
 function App(props) {
   return (
     <div>
-      <Provider store={store}>
+      {/* <Provider store={store}> */}
         <BrowserRouter>
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/stats" element={<Advert />} />
-            <Route path="/login" element={<Support />} />
+            <Route path="/advert" element={<Advert />} />
+            <Route path="/support" element={<Support />} />
           </Routes>
           <Footer />
         </BrowserRouter>
-      </Provider>
+      {/* </Provider> */}
     </div>
   );
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar(props) {
   return (
-    <div>
-      это навбар
-    </div>
+    <>
+      <nav>
+        <Link className="navbar-brand" to="/">
+          Главная
+        </Link>
+        <ul>
+          <li>
+            <Link  to="/">
+              Главная
+            </Link>
+          </li>
+          <li>
+            <Link to="/cards">
+              Курсы
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/command">
+              Вы научитесь
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/aboutus">
+              О нас
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
