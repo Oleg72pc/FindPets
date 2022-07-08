036aaa213a95dee511fb45f19721e3378e539be7
artfront5 <art.front@list.ru>
2022-07-08 14:50:24 +0300

    Open all cards, rotes in back, css, jsx adverts

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const { sequelize } = require('./db/models');
 const authRouter = require('./routes/auth.routes');
 const regRouter = require('./routes/reg.routes');
 const logRouter = require('./routes/log.routes');
+const adRouter = require('./routes/ad.routes');
 
 const app = express();
 const PORT = process.env.PORT ?? 4000;
@@ -15,6 +16,7 @@ config(app);
 app.use('/registration', regRouter);
 app.use('/login', logRouter);
 app.use('/auth', authRouter);
+app.use('/ad', adRouter);
 
 app.listen(PORT, async () => {
   console.log(`S E R V E R S T A R T E D A T ${PORT} P O R T`);
