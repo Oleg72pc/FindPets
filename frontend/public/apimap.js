668467bf45aab98f59b56a40a59f80812fd1e0c8
Oleg72pc <oleg72pc@mail.ru>
2022-07-11 16:59:11 +0300

    apimap/remove controls

--- frontend/public/apimap.js
+++ frontend/public/apimap.js
@@ -4,4 +4,12 @@ window.ymaps.ready(init);
             center: [59.93139123904442,30.41594565054736],
             zoom: 10
         });
+  myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+  myMap.controls.remove('searchControl'); // удаляем поиск
+  myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+  myMap.controls.remove('typeSelector'); // удаляем тип
+  // myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  myMap.controls.remove('rulerControl'); // удаляем контрол правил
+  // myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
     }
